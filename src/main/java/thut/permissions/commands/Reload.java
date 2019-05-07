f1dfac895963977367cb0245b0e5dd0f351936ba
Thutmose <elpatricimo@gmail.com>
2019-05-07 18:41:36 -0400

    Fixes crash on reloading perms

--- src/main/java/thut/permissions/commands/Reload.java
+++ src/main/java/thut/permissions/commands/Reload.java
@@ -4,6 +4,7 @@ import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
+import thut.permissions.GroupManager;
 import thut.permissions.ThutPerms;
 import thut.permissions.util.BaseCommand;
 
@@ -19,6 +20,7 @@ public class Reload extends BaseCommand
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
         ThutPerms.loadPerms();
+        GroupManager.get_instance()._server = server;
         sender.sendMessage(new TextComponentString("Reloaded Permissions from File"));
     }
 
